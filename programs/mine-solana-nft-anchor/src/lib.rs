7fffcb365dfd579febb17787a74629df7be8f5bc
David Anyatonwu <davidanyatonwu@gmail.com>
2023-11-06 14:11:13 +0100

    Alot

--- programs/mine-solana-nft-anchor/src/lib.rs
+++ programs/mine-solana-nft-anchor/src/lib.rs
@@ -1,15 +1,88 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{CreateMetadataAccountsV3, Metadata},
+    // token_interface::TokenAccount,
+    token::{Mint, Token, TokenAccount},
+};
+use mpl_token_metadata::pda::{find_master_edition_account, find_metadata_account};
 
 declare_id!("FoYSrfkztZJXMBa3Xv96JA7Khr5Tyccx6Q9Z4V8k7DQc");
 
 #[program]
 pub mod mine_solana_nft_anchor {
+    use anchor_spl::{
+        metadata::create_metadata_accounts_v3,
+        token::{mint_to, MintTo},
+    };
+    use mpl_token_metadata::state::DataV2;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<InitNFt>,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Result<()> {
+        // create mint account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
+            },
+        );
+        mint_to(cpi_context, 1)?;
+
+        // Create metadata account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                metadata: ctx.accounts.metadata_account.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                mint_authority: ctx.accounts.signer.to_account_info(),
+                update_authority: ctx.accounts.signer.to_account_info(),
+                payer: ctx.accounts.signer.to_account_info(),
+
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        );
+        let data_v2 = DataV2 {
+            name: name,
+            symbol: symbol,
+            uri: uri,
+            seller_fee_basis_points: 0,
+            creators: None,
+            collection: None,
+            uses: None,
+        };
+
+        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitNFt<'info> {
+    /// CHECK: ok, we are passing this account ourselves
+    #[account(mut, signer)]
+    pub signer: AccountInfo<'info>,
+    #[account(init, payer=signer, mint::decimals=0, mint::authority=signer.key(), mint::freeze_authority=signer.key())]
+    pub mint: Account<'info, Mint>,
+    #[account(init_if_needed, payer=signer, associated_token::mint=mint, associated_token::authority=signer)]
+    pub associated_token_account: Account<'info, TokenAccount>,
+    /// CHECK: address
+    #[account(mut, address=find_metadata_account(&mint.key()).0)]
+    pub metadata_account: AccountInfo<'info>,
+    /// CHECK: address
+    #[account(mut, address=find_master_edition_account(&mint.key()).0)]
+    pub master_edition_account: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
--- tests/mine-solana-nft-anchor.ts
+++ tests/mine-solana-nft-anchor.ts
@@ -1,13 +1,73 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MineSolanaNftAnchor } from "../target/types/mine_solana_nft_anchor";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import {
+  findMasterEditionPda,
+  findMetadataPda,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { publicKey } from "@metaplex-foundation/umi";
 
-describe("mine-solana-nft-anchor", () => {
+describe("mine-solana-nft-anchor", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace
+    .MineSolanaNftAnchor as Program<MineSolanaNftAnchor>;
 
-  const program = anchor.workspace.MineSolanaNftAnchor as Program<MineSolanaNftAnchor>;
+  const signer = provider.wallet;
 
+  const umi = createUmi("https://api.devnet.solana.com")
+    .use(walletAdapterIdentity(signer))
+    .use(mplTokenMetadata());
+
+  const mint = anchor.web3.Keypair.generate();
+
+  // deriving the associated token account for the mint account
+
+  const associatedTokenAccount = await getAssociatedTokenAddress(
+    mint.publicKey,
+    signer.publicKey
+  );
+
+  // Derive the metadata account
+  let metadataAccount = findMetadataPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  // derive the master edition pda
+
+  let masterEditionAccount = findMasterEditionPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  /*
+
+  const umi = createUmi();
+
+umi.use(nftStorageUploader({ token: "YOUR NFT.STORAGE API KEY" }));
+
+
+const imageBuffer = readFileSync('PATH TO IMAGE FILE')
+async function uploader() {
+  const [imageUri] = await umi.uploader.upload([
+    createGenericFile(imageBuffer, 'FILE NAME.PNG'),
+  ]);
+
+  // Upload the JSON metadata.
+  const uri = await umi.uploader.uploadJson({
+    name: 'NFT #1',
+    description: 'description 1',
+    image: imageUri,
+  })
+  console.log("uri", uri);
+}
+
+uploader();
+  */
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
